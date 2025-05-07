9a84e8d58a5e5b6cc95091f2bf746f24fe25790f
Pyssy_Destroyer <117829026+Umbassa-winner@users.noreply.github.com>
2025-05-07 15:08:39 +0300

    Все восстновил + начал делать ДЗ по 4 блоку

---
+++ main.py
@@ -0,0 +1,11 @@
+def double_elements(lst):
+    answer = []
+    
+    for point in lst:
+        point *= 2
+        answer.append(point)
+    
+    print(answer)
+    return answer
+
+double_elements([1, 2, 3])
